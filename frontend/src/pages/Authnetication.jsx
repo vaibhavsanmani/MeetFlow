4c2b5fa6cbe5a2dcdee6398b6ec766f390d213b6
vaibhavsanmani <vaibhavsanmani9@gmail.com>
2026-07-12 22:37:02 +0530

    debugging frontend

--- frontend/src/pages/Authnetication.jsx
+++ frontend/src/pages/Authnetication.jsx
@@ -16,9 +16,6 @@ import { AuthContext } from '../contexts/AuthContext';
 import { Snackbar } from '@mui/material';
 
 
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
 const defaultTheme = createTheme();
 
 export default function Authentication() {
